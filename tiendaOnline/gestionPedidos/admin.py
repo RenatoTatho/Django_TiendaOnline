73b30ba1b0125c151dd0e9ef3be100accb5a50a9
Renato Huanca <starboy.tatho0407@gmail.com>
2023-12-22 01:18:18 -0500

    Connfiguracion del Panel de Administrador
    mostrar las tablas que se habian creado,
    modificando los campos y colocar una barra de busqueda para los clientes.

--- tiendaOnline/gestionPedidos/admin.py
+++ tiendaOnline/gestionPedidos/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from gestionPedidos.models import *
+
 # Register your models here.
+
+class ClienteAdmin(admin.ModelAdmin):# ver que campos podemos observar en el administrador de la clase clientes
+    list_display = ('nombre','direccion','telefono')#para que aparezca los campos que deseamos observar en el panel del admin
+    search_fields = ('nombre','telefono')#es para poder buscar 
+
+
+admin.site.register(Cliente, ClienteAdmin)#tenemos que poner nuestra nueva clase 
+admin.site.register(Articulos)
+admin.site.register(Pedidos)
--- tiendaOnline/gestionPedidos/models.py
+++ tiendaOnline/gestionPedidos/models.py
@@ -4,14 +4,20 @@ from django.db import models
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length = 30)
-    direccion = models.CharField(max_length = 50)
-    email = models.EmailField()
+    direccion = models.CharField(max_length = 50, verbose_name = 'La direccion')
+    email = models.EmailField(blank = True, null = True)
     telefono = models.CharField(max_length = 10)
+
+    def __str__(self) -> str:
+        return self.nombre
 
 class Articulos(models.Model):
     nombre = models.CharField(max_length = 30)
     seccion = models.CharField(max_length = 20)
     precio = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f'[El nombre es: {self.nombre}, la seccion es {self.seccion}, y el precio es {self.precio}]'
 
 class Pedidos(models.Model):
     numero = models.IntegerField()
